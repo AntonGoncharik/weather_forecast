1b849bb889f35f3314ac707bb4db6945a43d2095
AntonGoncharik <ant.goncharik@gmail.com>
2019-10-31 23:43:11 +0300

    Updates

---
+++ precache-manifest.d87d0d140a7cc4870d9827808e2ec5ac.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b5e9e6c86b31e292dd35069ad279917",
+    "url": "/weather_forecast/index.html"
+  },
+  {
+    "revision": "fc82f3baa6c4a432fcff",
+    "url": "/weather_forecast/static/css/main.93f4927a.chunk.css"
+  },
+  {
+    "revision": "7a8c02cc38b9e37ed6c6",
+    "url": "/weather_forecast/static/js/2.cb8aec3f.chunk.js"
+  },
+  {
+    "revision": "fc82f3baa6c4a432fcff",
+    "url": "/weather_forecast/static/js/main.45853d3b.chunk.js"
+  },
+  {
+    "revision": "6c9d93678ca6ba2b52f5",
+    "url": "/weather_forecast/static/js/runtime-main.0465d3be.js"
+  },
+  {
+    "revision": "7e286ee7054fee2b9964f562c68be7c6",
+    "url": "/weather_forecast/static/media/c.7e286ee7.svg"
+  },
+  {
+    "revision": "6bd9a25a0f6d2f9763ffbc56be6f4086",
+    "url": "/weather_forecast/static/media/h.6bd9a25a.svg"
+  },
+  {
+    "revision": "3cfae17047d8cc4040613aa4d89fce21",
+    "url": "/weather_forecast/static/media/hc.3cfae170.svg"
+  },
+  {
+    "revision": "e4421ea02ac018394df8b656a0f0476e",
+    "url": "/weather_forecast/static/media/hr.e4421ea0.svg"
+  },
+  {
+    "revision": "751f7d15dd12489b674dd1ce1ec8f741",
+    "url": "/weather_forecast/static/media/lc.751f7d15.svg"
+  },
+  {
+    "revision": "8d24156b4b49ada79f6d6addd92f9c70",
+    "url": "/weather_forecast/static/media/lr.8d24156b.svg"
+  },
+  {
+    "revision": "216dd7511ffd7cc251d5a28d723c75fc",
+    "url": "/weather_forecast/static/media/pre.216dd751.webp"
+  },
+  {
+    "revision": "12c99f3cbcde1731cef43c8a3985b354",
+    "url": "/weather_forecast/static/media/s.12c99f3c.svg"
+  },
+  {
+    "revision": "4729dec81dae34307d842e2e48906709",
+    "url": "/weather_forecast/static/media/sl.4729dec8.svg"
+  },
+  {
+    "revision": "8e64c51e1e5b1b19ef8d6329bf2e6588",
+    "url": "/weather_forecast/static/media/sn.8e64c51e.svg"
+  },
+  {
+    "revision": "0228ea650e4f414667cead94013bf730",
+    "url": "/weather_forecast/static/media/t.0228ea65.svg"
+  }
+]);
